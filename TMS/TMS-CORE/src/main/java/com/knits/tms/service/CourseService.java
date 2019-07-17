d910ffd7adf7737f4f53f657431fdf11a4eb5006
antti.kask <external.antti.kask@kuehne-nagel.com>
2019-07-17 16:32:38 +0300

    TMS-004 View courses

--- TMS/TMS-CORE/src/main/java/com/knits/tms/service/CourseService.java
+++ TMS/TMS-CORE/src/main/java/com/knits/tms/service/CourseService.java
@@ -1,12 +1,18 @@
 package com.knits.tms.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.knits.tms.beans.CourseDto;
+import com.knits.tms.beans.LectureDto;
+import com.knits.tms.beans.LectureSearchDto;
 import com.knits.tms.beans.ModuleDto;
 import com.knits.tms.dao.CourseDao;
 import com.knits.tms.dao.ModuleDao;
 import com.knits.tms.model.Course;
+import com.knits.tms.model.Lecture;
 import com.knits.tms.model.Module;
 import com.knits.tms.util.BeanMappingUtils;
 
@@ -20,6 +26,11 @@ public class CourseService {
 		Course courseAsModel= BeanMappingUtils.dto2Model(courseDto);
 		courseDao.save(courseAsModel);		
 	}
-
+	
+	public List<Course> viewAllCourses(){
+		List<Course> courses = courseDao.listAll();
+		
+		return courses;
+	}
 
 }
